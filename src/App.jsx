80682d2cf3c20390bfca04dc1ba6740aefae66e3
sanjaysingh24 <sanjubora84@gmail.com>
2025-06-05 22:45:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import Cookies from 'js-cookie';
-import axios from 'axios'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
-const handleclick = ()=>{
-Cookies.set('heytoken', 'ram', {
-  path: '/',
-  expires: 7,
-  secure: true,           // IMPORTANT for HTTPS
-  sameSite: 'None'        // IMPORTANT for cross-site cookie
-});
-}
-const checkCookies = async()=>{
-  try{
-   const sendresponse = await axios.get('https://checkbackend-90i0.onrender.com/',{},{
-     withCredentials: true
-   })
-  }catch(err){
-    console.log(err)
-  }
+  const [count, setCount] = useState(0);
 
-}
+  const handleclick = () => {
+    Cookies.set('heytoken', 'ram', {
+      path: '/',
+      expires: 7,
+      secure: true,
+      sameSite: 'None'
+    });
+    alert('Cookie set');
+  };
+
+  const checkCookies = async () => {
+    try {
+      const sendresponse = await axios.get(
+        'https://checkbackend-90i0.onrender.com/',
+        {
+          withCredentials: true
+        }
+      );
+      console.log(sendresponse.data);
+      alert(sendresponse.data.message || 'Cookie checked');
+    } catch (err) {
+      console.log(err);
+      alert('Error checking cookie');
+    }
+  };
+
   return (
     <>
-      
-     <div>
-          <button onClick={handleclick}>set cookies</button>
-          <button onClick={checkCookies}>checkcookies</button>
-     </div>
+      <div>
+        <button onClick={handleclick}>Set Cookie</button>
+        <button onClick={checkCookies}>Check Cookie</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
